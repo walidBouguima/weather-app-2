47405bb32abedc80f53a024c2702f19c809549c1
Walid Bouguima <walid.bouguima@gmail.com>
2020-10-03 15:47:30 +0100

    Provide the fake service class and use it instead of HttpClientTestModule fake

--- src/app/weather/weather.service.spec.ts
+++ src/app/weather/weather.service.spec.ts
@@ -1,13 +1,16 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { TestBed } from '@angular/core/testing'
 
 import { WeatherService } from './weather.service'
+import { WeatherServiceFake } from './weather.service.fake'
 
 describe('WeatherService', () => {
   let service: WeatherService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] })
+    TestBed.configureTestingModule({
+      // imports: [HttpClientTestingModule],
+      providers: [{ provide: WeatherService, useClass: WeatherServiceFake }],
+    })
     service = TestBed.inject(WeatherService)
   })
 
