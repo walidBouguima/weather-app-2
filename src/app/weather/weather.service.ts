58c04dcd8ef9374c437e6d4a0063f08335ba1d33
Walid Bouguima <walid.bouguima@gmail.com>
2020-10-03 06:59:37 +0100

    Create the getCurrentWeather function using http params

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpParams } from '@angular/common/http'
 import { Injectable } from '@angular/core'
+import { environment } from 'src/environments/environment'
 
 interface ICurrentWeatherData {
   weather: [
@@ -22,4 +23,14 @@ interface ICurrentWeatherData {
 })
 export class WeatherService {
   constructor(private httpClient: HttpClient) {}
+
+  getCurrentWeather(city: string, country: string) {
+    const uriParams = new HttpParams()
+      .set('q', `${city}, ${country}`)
+      .set('appid', environment.appId)
+    return this.httpClient.get<ICurrentWeatherData>(
+      `${environment.baseUrl}api.openweather.org/data/2.5/weather?`,
+      { params: uriParams }
+    )
+  }
 }
