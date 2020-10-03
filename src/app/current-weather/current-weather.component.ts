048b84e0faae6114cbfcd21c013fabb7964d1aea
Walid Bouguima <walid.bouguima@gmail.com>
2020-10-03 08:29:38 +0100

    Change the type of data from Date to number

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 
 import { ICurrentWeather } from '../interfaces'
+import { WeatherService } from '../weather/weather.service'
 
 @Component({
   selector: 'app-current-weather',
@@ -9,17 +10,11 @@ import { ICurrentWeather } from '../interfaces'
 })
 export class CurrentWeatherComponent implements OnInit {
   current: ICurrentWeather
-  constructor() {
-    this.current = {
-      city: 'Baghdad',
-      country: 'Iraq',
-      date: new Date(),
-      image: 'assets/img/sunny.svg',
-      temperature: 72,
+  constructor(private weatherService: WeatherService) {}
 
-      description: 'sunny',
-    } as ICurrentWeather
+  ngOnInit(): void {
+    this.weatherService
+      .getCurrentWeather('Bethesda', 'US')
+      .subscribe((data) => (this.current = data))
   }
-
-  ngOnInit(): void {}
 }
