fcdd7bb349f6e7aa92b7c9b39659e85fba462c04
Walid Bouguima <walid.bouguima@gmail.com>
2020-10-03 08:29:14 +0100

    Solve type mismatching and convert kelvin to fahrenheit

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,6 +1,10 @@
 import { HttpClient, HttpParams } from '@angular/common/http'
 import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
 import { environment } from 'src/environments/environment'
+
+import { ICurrentWeather } from '../interfaces'
 
 interface ICurrentWeatherData {
   weather: [
@@ -24,13 +28,30 @@ interface ICurrentWeatherData {
 export class WeatherService {
   constructor(private httpClient: HttpClient) {}
 
-  getCurrentWeather(city: string, country: string) {
+  getCurrentWeather(city: string, country: string): Observable<ICurrentWeather> {
     const uriParams = new HttpParams()
       .set('q', `${city}, ${country}`)
       .set('appid', environment.appId)
-    return this.httpClient.get<ICurrentWeatherData>(
-      `${environment.baseUrl}api.openweather.org/data/2.5/weather?`,
-      { params: uriParams }
-    )
+    return this.httpClient
+      .get<ICurrentWeatherData>(
+        `${environment.baseUrl}api.openweathermap.org/data/2.5/weather`,
+        { params: uriParams }
+      )
+      .pipe(map((data) => this.transformToICurrentWeather(data)))
+  }
+
+  transformToICurrentWeather(data: ICurrentWeatherData): ICurrentWeather {
+    return {
+      city: data.name,
+      country: data.sys.country,
+      date: data.dt * 1000,
+      image: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`,
+      temperature: this.convertKelvinToFahrenheit(data.main.temp),
+      description: data.weather[0].description,
+    }
+  }
+
+  convertKelvinToFahrenheit(kelvin: number): number {
+    return (kelvin * 9) / 5 - 459.67
   }
 }
