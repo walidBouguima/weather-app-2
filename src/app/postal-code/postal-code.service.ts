b140fb7538af90f77780be258dd22b6c9c02008b
Walid Bouguima <walid.bouguima@gmail.com>
2020-10-06 21:16:06 +0100

    Add a new component named postal code service and set up the API call function and interfaces. Additionaly I updated the environment to add two new parameters

---
+++ src/app/postal-code/postal-code.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient, HttpParams } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { defaultIfEmpty, flatMap } from 'rxjs/operators'
+import { environment } from 'src/environments/environment'
+
+export interface IPostalCode {
+  countryCode: string
+  postalCode: string
+  placeName: string
+  lng: number
+  lat: number
+}
+export interface IPostalCodeData {
+  postalCodes: [IPostalCode]
+}
+export interface IPostalCodeService {
+  resolvePostalCode(postalCode: string): Observable<IPostalCode>
+}
+@Injectable({
+  providedIn: 'root',
+})
+export class PostalCodeService {
+  constructor(private httpClient: HttpClient) {}
+
+  resolvePostalCode(postalCode: string): Observable<IPostalCode> {
+    const uriParams = new HttpParams()
+      .set('maxRows', '1')
+      .set('username', environment.username)
+      .set('postalcode', postalCode)
+    return this.httpClient
+      .get<IPostalCodeData>(
+        `${environment.baseUrl}${environment.geonamesApi}.geonames.org/postalCodeSearchJSON`,
+        { params: uriParams }
+      )
+      .pipe(
+        // tslint:disable-next-line: deprecation
+        flatMap((data) => data.postalCodes),
+        defaultIfEmpty(null)
+      )
+  }
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,6 @@ export const environment = {
   production: true,
   appId: 'a19d7c33fd81c08f642dafca83ea8654',
   baseUrl: 'https://cors-anywhere.herokuapp.com/https://',
+  username: 'walidbougusssssssima',
+  geonamesApi: 'secure',
 }
