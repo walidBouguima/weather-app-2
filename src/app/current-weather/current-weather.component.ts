b281e1a381b2dc4960fafa01496bdbc4cd6b0754
Walid Bouguima <walid.bouguima@gmail.com>
2020-10-06 19:49:46 +0100

    Clean the code in the current weather comonent logic by leveraging the reactive programming with async. This entailed some changes in the template as well

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core'
-import { SubSink } from 'subsink'
+import { Component } from '@angular/core'
+import { Observable } from 'rxjs'
 
 import { ICurrentWeather } from '../interfaces'
 import { WeatherService } from '../weather/weather.service'
@@ -9,20 +9,10 @@ import { WeatherService } from '../weather/weather.service'
   templateUrl: './current-weather.component.html',
   styleUrls: ['./current-weather.component.css'],
 })
-export class CurrentWeatherComponent implements OnInit, OnDestroy {
-  private subscriptions = new SubSink()
-  current: ICurrentWeather
-  constructor(private weatherService: WeatherService) {}
-
-  ngOnInit(): void {
-    this.subscriptions.add(
-      this.weatherService.currentWeather$.subscribe((data) => (this.current = data))
-    )
-  }
-
-  // tslint:disable-next-line: typedef
-  ngOnDestroy() {
-    this.subscriptions.unsubscribe()
+export class CurrentWeatherComponent {
+  current$: Observable<ICurrentWeather>
+  constructor(private weatherService: WeatherService) {
+    this.current$ = this.weatherService.currentWeather$
   }
   // tslint:disable-next-line: typedef
   getOrdinal(date: number) {
